3719bb6f4d43bb18fc0d84861146732a1a8c6f84
Chinmay Patil <75988613+chinmay7016@users.noreply.github.com>
2025-02-05 00:41:24 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./Navbar.css"; // Import custom styles
+
+const NavbarComponent = () => {
+  const [activeLink, setActiveLink] = useState("home");
+
+  return (
+    <Navbar expand="md" className="crazy-navbar">
+      <Container>
+        <Navbar.Brand href="/" className="logo">🚀 CrazyNav</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {["home", "skills", "projects", "contact"].map((item) => (
+              <Nav.Link
+                key={item}
+                href={`#${item}`}
+                className={`crazy-link ${activeLink === item ? "active" : ""}`}
+                onClick={() => setActiveLink(item)}
+              >
+                {item.charAt(0).toUpperCase() + item.slice(1)}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
