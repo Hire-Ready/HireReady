72132bdb1d84b504c021c786035fc38cdcfd12c7
RAJAS DEV <rajasdev@gmail.com>
2025-03-20 12:33:46 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,24 @@
-import { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import "./Navbar.css"; // Import custom styles
+import React from 'react';
+import './Navbar.css';
 
-const NavbarComponent = () => {
-  const [activeLink, setActiveLink] = useState("home");
-
+const Navbar = () => {
   return (
-    <Navbar expand="md" className="crazy-navbar">
-      <Container>
-        <Navbar.Brand href="/" className="logo">🚀 CrazyNav</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {["home", "skills", "projects", "contact"].map((item) => (
-              <Nav.Link
-                key={item}
-                href={`#${item}`}
-                className={`crazy-link ${activeLink === item ? "active" : ""}`}
-                onClick={() => setActiveLink(item)}
-              >
-                {item.charAt(0).toUpperCase() + item.slice(1)}
-              </Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="navbar-logo">HireReady</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#pricing">Pricing</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#faq">FAQ</a></li>
+      </ul>
+      <div className="navbar-buttons">
+        <a href="#login" className="login-btn">Login</a>
+        <a href="#signup" className="signup-btn">Sign Up</a>
+      </div>
+    </nav>
   );
 };
 
-export default NavbarComponent;
+export default Navbar;
